9677458486508aaa4fa6c0e63701bd15a5a44aa0
Enitoni <enitoni@gmail.com>
2022-06-14 04:52:11 +0200

    Remove unecessary mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,15 @@
 mod grouping;
 mod memory;
 
-mod rollercoaster {
-    use crate::memory::*;
+use crate::memory::*;
 
-    pub trait Rollercoaster: Iterator
-    where
-        Self: Sized,
-    {
-        fn memory(self) -> Memory<Self> {
-            Memory::new(self)
-        }
+pub trait Rollercoaster: Iterator
+where
+    Self: Sized,
+{
+    fn memory(self) -> Memory<Self> {
+        Memory::new(self)
     }
-
-    impl<T: Iterator> Rollercoaster for T {}
 }
 
-pub use crate::rollercoaster::Rollercoaster;
+impl<T: Iterator> Rollercoaster for T {}
