e40e67989fa8caa4aa0fb0f04102ecf03d3a9aa1
Enitoni <enitoni@gmail.com>
2022-06-14 05:38:46 +0200

    add documentation for `memory()`

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,37 @@ pub trait Rollercoaster: Iterator
 where
     Self: Sized,
 {
+    /// Creates an iterator that allows remembering values
+    /// for the next iteration.
+    ///
+    /// This is useful in situations where you need to:
+    /// - Read from the item, then use it the next iteration.
+    /// - Insert items returned by the iterator.
+    ///
+    /// ## How is this different from `peekable()`?
+    /// [Memory] is more flexible because unlike `Peekable`,
+    /// it allows you to own the value you are working with
+    /// and decide if you need to use it the next iteration.
+    ///
+    /// Since `remember()` only takes owned values, you avoid
+    /// dealing with references and it can make the iterator easier to work with.
+    ///
+    /// # Example
+    /// ```
+    /// # use rollercoaster::Rollercoaster;
+    /// #
+    /// let mut nums = vec![1, 2, 3, 4, 5].into_iter().memory();
+    ///
+    /// for n in nums.by_ref() {
+    ///     if n == 4 {
+    ///         nums.remember(n);
+    ///         break;
+    ///     }
+    /// }
+    ///
+    /// let summed: u32 = nums.sum();
+    /// assert_eq!(summed, 9);
+    /// ```
     fn memory(self) -> Memory<Self> {
         Memory::new(self)
     }
