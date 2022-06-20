c5901e812d6e68e313496b73bea4a7cab9680b44
Enitoni <enitoni@gmail.com>
2022-06-20 09:40:32 +0200

    Add documentation for the trait

--- src/macros.rs
+++ src/macros.rs
@@ -24,7 +24,28 @@ macro_rules! rollercoaster {
             use crate::$name::*;
         )*
 
-
+        /// Adds useful extension iterators for any [Iterator].
+        ///
+        /// Please consult the documentation for more information.
+        ///
+        /// # Usage
+        /// ```
+        /// use rollercoaster::Rollercoaster;
+        ///
+        /// // Now we can use the extra
+        /// // iterator extensions on any iterator.
+        /// let result: Vec<_> = [1, 2, 2, 3, 4, 5]
+        ///     .into_iter()
+        ///     .unique()
+        ///     .group_by(|x| *x > 3)
+        ///     .map(|g| g.items)
+        ///     .collect();
+        ///
+        /// assert_eq!(result, vec![
+        ///     vec![1, 2, 3],
+        ///     vec![4, 5]
+        /// ])
+        /// ```
         pub trait Rollercoaster: Iterator
         where
             Self: Sized,
